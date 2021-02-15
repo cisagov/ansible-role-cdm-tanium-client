b3831df37420177f56ec6b02ea22e193cfc8fb35
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-02-15 14:23:13 -0500

    Configure TF code for this Ansible role

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,12 +10,42 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "production_bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket where the production Tanium system packages live."
+  default     = "cisa-cool-third-party-production"
+}
+
+variable "production_objects" {
+  type        = list(string)
+  description = "The Tanium system package objects inside the production bucket."
+  default = [
+    "burpsuite_pro.license",
+    "burpsuite_pro_linux_v2020_11.sh",
+  ]
+}
+
+variable "staging_bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket where the staging Tanium system packages live."
+  default     = "cisa-cool-third-party-staging"
+}
+
+variable "staging_objects" {
+  type        = list(string)
+  description = "The Tanium system packages inside the staging bucket."
+  default = [
+    "burpsuite_pro.license",
+    "burpsuite_pro_linux_v2020_11.sh",
+  ]
+}
+
 variable "tags" {
   type        = map(string)
   description = "Tags to apply to all AWS resources created"
 
   default = {
     Team        = "VM Fusion - Development"
-    Application = "ansible-role-tanium testing"
+    Application = "ansible-role-venom-tanium testing"
   }
 }
